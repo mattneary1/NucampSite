8bee6a9beac6d9bc1c58979647f1efae994eb1f1
Matthew Neary <mattneary1@gmail.com>
2022-09-25 08:51:09 -0700

    Destructuring and code cleanup.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,19 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
-import { Col, Row } from 'reactstrap';
-import CampsiteCard from './CampsiteCard';
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { Col, Row } from "reactstrap";
+import CampsiteCard from "./CampsiteCard";
 
 const CampsitesList = () => {
   return (
-    <Row className='ms-auto'>
+    <Row className="ms-auto">
       {CAMPSITES.map((campsite) => {
         return (
-          <Col md='5' className='m-4' key={campsite.id}>
+          <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
           </Col>
-        )
+        );
       })}
     </Row>
-  )
-}
+  );
+};
 
 export default CampsitesList;
