d090047372242d63cf589f8e7b245885a3e1d9f5
Matthew Neary <mattneary1@gmail.com>
2022-09-29 09:49:16 -0700

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -12,3 +12,8 @@ export const selectAllCampsites = () => {
 export const selectCampsiteById = (id) => {
   return CAMPSITES.find((campsite) => campsite.id === id);
 };
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
+};
+
