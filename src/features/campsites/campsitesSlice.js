939113aeaea5a1ee0ee0195ad96890f33ee809db
Matthew Neary <mattneary1@gmail.com>
2022-09-29 16:09:34 -0700

    CampsiteDetailPage with useParams

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,19 +1,13 @@
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
-import CampsitesList from "./CampsitesList";
 
 export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
-// export const selectRandomCampsite = () => {
-//   return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
-
 export const selectCampsiteById = (id) => {
-  return CAMPSITES.find((campsite) => campsite.id === id);
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
   return CAMPSITES.find((campsite) => campsite.featured);
 };
-
